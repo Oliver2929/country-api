72715f7512fa8098cb7c65145b7fed04781eccf2
fowosere ademola <fowosereademola@gmail.com>
2025-10-25 19:56:15 +0100

    feat: update of app module to handle railway integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,13 +10,19 @@ import { CountryModule } from './country/module/country.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT || '3306'),
       username: process.env.DB_USERNAME || 'root',
       password: process.env.DB_PASSWORD || '',
       database: process.env.DB_NAME || 'country_cache',
       autoLoadEntities: true,
       synchronize: true,
+      ssl:
+        process.env.DB_SSL === 'true'
+          ? { rejectUnauthorized: false }
+          : undefined,
+      retryAttempts: 10,
+      retryDelay: 5000,
     }),
     CountryModule,
   ],
